c090ef70260f42cf4df2bbfa2f78e7615d554a2a
aqillarossa <rossaaqila8@gmail.com>
2024-03-07 09:08:30 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus corporis ullam nostrum. Dolorum pariatur nisi odio rem rerum, quidem beatae? Totam reiciendis perspiciatis dolore harum deleniti cupiditate at molestias iure?</p>
 </body>
 </html>
